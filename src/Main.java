2a0712bb63970708bdf23ef064c4fcae2173f13b
riphel05 <phelpsre@mail.uc.edu>
2022-09-25 22:27:23 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class
+Main {
+    public static void main(String[] args) {
+        String menu = "D - Democrat R - Republican I - Independent O - Other ";
+        String menuChoice = "D";
+
+        if(menuChoice.equalsIgnoreCase("D"))
+        {
+            System.out.println("You chose Democrat, you get an elephant.");
+
+        }
+        else-if(menuChoice.equalsIgnoreCase("R"))
+        {
+            System.out.println("You chose Republican, you get a Donkey.");
+        }
+        else-if(menuChoice.equalsIgnoreCase("I"))
+        {
+            System.out.println("You chose Independent, you get a man.");
+        }
+        else-if(menuChoice.equalsIgnoreCase("O"))
+        {
+            System.out.println("You chose Other, you get a thought bubble");
+        }
+
+    }
+
+}
