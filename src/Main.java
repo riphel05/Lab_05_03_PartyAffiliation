14ef0909ed63fe99e0b412bc40d093ec7d745636
riphel05 <phelpsre@mail.uc.edu>
2022-09-25 22:31:50 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@ public class
 Main {
     public static void main(String[] args) {
         String menu = "D - Democrat R - Republican I - Independent O - Other ";
-        String menuChoice = "D";
+        String menuChoice = "O";
 
         if(menuChoice.equalsIgnoreCase("D"))
         {
             System.out.println("You chose Democrat, you get an elephant.");
 
         }
-        else-if(menuChoice.equalsIgnoreCase("R"))
+        else if(menuChoice.equalsIgnoreCase("R"))
         {
             System.out.println("You chose Republican, you get a Donkey.");
         }
-        else-if(menuChoice.equalsIgnoreCase("I"))
+        else if(menuChoice.equalsIgnoreCase("I"))
         {
             System.out.println("You chose Independent, you get a man.");
         }
-        else-if(menuChoice.equalsIgnoreCase("O"))
+        else if(menuChoice.equalsIgnoreCase("O"))
         {
             System.out.println("You chose Other, you get a thought bubble");
         }
